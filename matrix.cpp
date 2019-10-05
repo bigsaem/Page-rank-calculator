cd0b115b67e78c57967a8ed7f9410f3d7008feea
Sammy <bigsaem@gmail.com>
2019-10-05 14:13:50 -0700

    Initial commit

---
+++ matrix.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Sammy on 2019-10-01.
+//
+#include <vector>
+#include "matrix.hpp"
+using namespace std;
+
+class matrix{
+
+private:
+    int n;
+    int [][] matrix;
+    vector<vector<double>> square;
+public:
+    matrix(int n= 1 ):n(n), square[n-1][n-1]{0.0}{
+        square[n-1][n-1] = 0.0;
+    }
+
+};
---
+++ matrix.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Sammy on 2019-10-01.
+//
+
+#ifndef ASSIGNMENT1_MATRIX_HPP
+#define ASSIGNMENT1_MATRIX_HPP
+
+#endif //ASSIGNMENT1_MATRIX_HPP
